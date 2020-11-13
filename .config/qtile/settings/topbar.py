95ab9cc2f0d7041e6f085de072f8c8953191f970
josema-planes <73686197+josema-planes@users.noreply.github.com>
2020-11-13 21:23:15 +0100

    Update topbar.py

--- .config/qtile/settings/topbar.py
+++ .config/qtile/settings/topbar.py
@@ -1,21 +1,10 @@
+from libqtile import bar, layout, widget
 from libqtile.config import Screen
-from libqtile import bar, widget
-import subprocess
-
-colors = [
-    "#FFFFFF", #white
-    "#000000", #black
-    "#9b9b9b", #grey
-    "#FF0000", #red
-    "#00FF00", #green
-    "#0000FF", #blue
-    ]
 
 widget_defaults = dict(
-    font="sans",
-    fontsize = 18,
-    padding = 2,
-    background=colors[2]
+    font='UbuntuMono Nerd Font Bold',
+    fontsize=22,
+    padding=4,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -23,91 +12,48 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.Sep(
-                        linewidth = 0,
-                        padding = 6,
-                        ),
-               widget.GroupBox(
-                        fontsize = 18,
-                        margin_y = 0, 
-                        margin_x = 0, 
-                        padding_y = 9, 
-                        padding_x = 5, 
-                        borderwidth = 4, 
-                        active = colors[0], 
-                        inactive = colors[2],
-                        rounded = False,
-                        highlight_method = "line",
-                        this_current_screen_border = colors[3]
-                        ),
-                widget.Sep(
-                        linewidth = 0,
-                        padding = 10,
-                        ),
-                widget.WindowName(
-                        fontsize = 14,
-                        padding = 6
-                        ),
-                widget.Systray(),
-                widget.Sep(
-                        linewidth = 0,
-                        padding = 10,
-                        ),
-                widget.TextBox(
-                        text="│", 
-                        padding = 6,
-                        fontsize=18
-                        ),
-                widget.TextBox(
-                        text="⌂", 
-                        padding = 6,
-                        fontsize=18
-                        ),
-                widget.CurrentLayout(
-                        fontsize=18,
-                        ),
-                widget.TextBox(
-                        text="│", 
-                        padding = 6,
-                        fontsize=18
-                        ),
-                widget.TextBox(
-                        text="☼",
-                        padding = 6,
-                        fontsize=22,
-                        ),
-                widget.Clock(
-                        format="%A, %B %d - %H:%M",
-                        fontsize=18,
-                        ),
-                widget.TextBox(
-                        text="│", 
-                        padding = 6,
-                        fontsize=18
-                        ),
-                widget.TextBox(
-                      text = "♪",
-                       padding = 0,
-                       fontsize=20,
-                       ),
-                widget.Volume(
-                        padding = 5,
-                        fontsize=18,
-                        ),
-                widget.TextBox(
-                        text="│", 
-                        padding = 6,
-                        fontsize=18,
-                        ),
-                widget.TextBox(
-                        text="↯",
-                        fontsize=22,
-                        padding = 0,
-                        ),
+                widget.GroupBox(
+                    fontsize = 40,
+                    padding_y = 5,
+                    padding = 10,
+                    borderwidth = 3,
+                    active = "#FFFFFF",
+                    rounded = False,
+                    highlight_method = "line",
+                    this_current_screen_border = "#FF0000",
+                    disable_drag=True
+                    ),
+
+                widget.Prompt(),
+
+                widget.WindowName(fontsize = 14),
+
+                widget.Net(
+                    interface = "wlo1",
+                    format = '{down}↓{up}↑',
+                    ),
+
+                widget.TextBox(text = "| "),
+                widget.Clock(format='%I:%M %p'),
+
+                widget.TextBox(text = "| ♪"),
+                widget.Volume(),
+
+                widget.TextBox(text = "|"),
+
                 widget.Battery(
-                        fontsize=18,
-                        format='{percent:2.0%}',
-                        ),
+                    format = '{char} {percent:2.0%}',
+                    charge_char = "⚡ ",
+                    discharge_char = " ",
+                    notify_below = 20,
+                    low_foreground = 'FF0000',
+                    low_percentage = 0.3,
+                    update_interval = 30,
+                    ),
+                
+                widget.TextBox(text = "|"),
+                
+                widget.Systray(),
             ],
             24,
         ),
