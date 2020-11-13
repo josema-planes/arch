eb0a0f55d02900addeffe094a90674f2d1eda3ec
josema-planes <73686197+josema-planes@users.noreply.github.com>
2020-11-13 21:24:32 +0100

    Update keys.py

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -1,68 +1,30 @@
+from libqtile.lazy import lazy
 from libqtile.config import Key
-from libqtile.command import lazy
-
 
 mod = "mod4"
 terminal = "alacritty"
 
 keys = [
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod, "shift"], "q", lazy.window.kill()), # Kill actual window
 
-    Key([mod, 'shift'], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "shift"], "r", lazy.restart()), # Restart qtile
 
-    Key([mod, "shift"], "r", lazy.restart(), desc="Restart qtile"),
-    
-    # Launch GUI apps
-    Key(
-        [mod], "m", 
-        lazy.spawn("firefox")
-        ),
-    Key(
-        [mod], "c", 
-        lazy.spawn("code")
-        ),
-    Key(
-        [mod], "n", 
-        lazy.spawn("nautilus")
-        ),
-    Key(
-        [mod], "z", 
-        lazy.spawn("zoom")
-        ),
-    Key(
-        [mod], "v", 
-        lazy.spawn("virtualbox")
-        ),
-    Key(
-        [mod], "l", 
-        lazy.spawn("libreoffice")
-        ),
-    Key(
-        [mod], "d", 
-        lazy.spawn("dmenu_run")
-        ),
+    Key([mod], "r", lazy.spawncmd()), # Open promp in bar
+
+    # Launch programs
+    Key([mod], "Return", lazy.spawn(terminal)),
+    Key([mod], "m", lazy.spawn("firefox")),
+    Key([mod], "c", lazy.spawn("code")),
+    Key([mod], "d", lazy.spawn("dmenu_run")),
+    Key([mod], "n", lazy.spawn("nautilus")),
+    Key([mod], "v", lazy.spawn("virtualbox")),
 
     # Volume
-    Key(
-        [], "XF86AudioRaiseVolume", 
-        lazy.spawn("amixer set Master 5%+")
-        ),
-    Key(
-        [], "XF86AudioLowerVolume", 
-        lazy.spawn("amixer set Master 5%-")
-        ),
-    Key(
-        [], "XF86AudioMute", 
-        lazy.spawn("amixer set Master 0%")
-        ),
-    
+    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set Master 5%-")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set Master 5%+")),
+    Key([], "XF86AudioMute", lazy.spawn("amixer set Master 0%")),
+
     # Brightness
-    Key(
-        [], "XF86MonBrightnessUp", 
-        lazy.spawn("brightnessctl set 10%+")
-        ),
-    Key(
-        [], "XF86MonBrightnessDown", 
-        lazy.spawn("brightnessctl set 10%-")
-        ),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set 10%+")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
 ]
