c59b78e1cc91b67a541d38474336290d33547158
josemapt <73686197+josemapt@users.noreply.github.com>
2020-11-14 20:49:06 +0100

    Update topbar.py

--- .config/qtile/settings/topbar.py
+++ .config/qtile/settings/topbar.py
@@ -17,7 +17,7 @@ screens = [
                     padding_y = 5,
                     padding = 10,
                     borderwidth = 3,
-                    active = "#FFFFFF",
+                    active = "#AAAAAA",
                     rounded = False,
                     highlight_method = "line",
                     this_current_screen_border = "#FF0000",
@@ -26,34 +26,92 @@ screens = [
 
                 widget.Prompt(),
 
-                widget.WindowName(fontsize = 14),
+                widget.WindowName(
+                    fontsize = 14,
+                    foreground = "#a151d3",
+                    ),
+
+                 widget.TextBox(
+                    text=" ",
+                    fontsize=37,
+                    padding=-2,
+                    background = "#000000",
+                    foreground = "#fb9f7f",
+                    ),
+
+                widget.CurrentLayout(
+                    background = "#fb9f7f",
+                ),
+
+                widget.TextBox(
+                    text=" ",
+                    fontsize=37,
+                    padding=-2,
+                    background = "#fb9f7f",
+                    foreground = "#F07178",
+                    ),
+                
+                widget.TextBox(
+                    text = "",
+                    fontsize = 16,
+                    background = "#F07178",
+                ),
 
                 widget.Net(
                     interface = "wlo1",
                     format = '{down}↓{up}↑',
+                    background = "#F07178",
                     ),
 
-                widget.TextBox(text = "| "),
-                widget.Clock(format='%I:%M %p'),
+                widget.TextBox(
+                    text=" ",
+                    fontsize=37,
+                    padding=-2,
+                    background = "F07178",
+                    foreground = "#a151d3",
+                    ),
 
-                widget.TextBox(text = "| ♪"),
-                widget.Volume(),
+                widget.TextBox(
+                    text = " ",
+                    fontsize = 16,
+                    background = "#a151d3",
+                ),
 
-                widget.TextBox(text = "|"),
+                widget.Clock(
+                    format = '%I:%M %p',
+                    background = "#a151d3",
+                    ),
 
-                widget.Battery(
-                    format = '{char} {percent:2.0%}',
-                    charge_char = "⚡ ",
-                    discharge_char = " ",
-                    notify_below = 20,
-                    low_foreground = 'FF0000',
-                    low_percentage = 0.3,
-                    update_interval = 30,
+                #widget.TextBox(text = "| ♪"),
+                #widget.Volume(),
+
+                #widget.TextBox(
+                #    text = "| ",
+                #    fontsize = 16
+                #    ),
+
+                #widget.Battery(
+                #    format = '{char} {percent:2.0%}',
+                #    charge_char = "⚡",
+                #    discharge_char = " ",
+                #    notify_below = 20,
+                #    low_foreground = 'FF0000',
+                #    low_percentage = 0.3,
+                #    update_interval = 30,
+                #    ),
+
+                widget.TextBox(
+                    text=" ",
+                    fontsize=37,
+                    padding=-2,
+                    background = "#a151d3",
+                    foreground = "#000000",
                     ),
                 
-                widget.TextBox(text = "|"),
-                
-                widget.Systray(),
+                widget.Systray(
+                    icon_size = 24,
+                    padding = 8,
+                ),
             ],
             24,
         ),
