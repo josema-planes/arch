8636a3fa7239cccd0e63e5fca5def59f7ce1f4f4
josemapt <73686197+josemapt@users.noreply.github.com>
2020-11-14 20:49:54 +0100

    Update keys.py

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -5,11 +5,16 @@ mod = "mod4"
 terminal = "alacritty"
 
 keys = [
+    # Change focused window
+    Key([mod], "Tab",  lazy.layout.next()),
+
+    #-----------------------------------------------------
     Key([mod, "shift"], "q", lazy.window.kill()), # Kill actual window
 
     Key([mod, "shift"], "r", lazy.restart()), # Restart qtile
 
     Key([mod], "r", lazy.spawncmd()), # Open promp in bar
+    #------------------------------------------------------
 
     # Launch programs
     Key([mod], "Return", lazy.spawn(terminal)),
@@ -18,13 +23,17 @@ keys = [
     Key([mod], "d", lazy.spawn("dmenu_run")),
     Key([mod], "n", lazy.spawn("nautilus")),
     Key([mod], "v", lazy.spawn("virtualbox")),
+    Key([mod], "l", lazy.spawn("libreoffice")),
 
     # Volume
-    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set Master 5%-")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set Master 5%+")),
-    Key([], "XF86AudioMute", lazy.spawn("amixer set Master 0%")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("/home/josema/scripts/volume.sh down")),
+    Key([],"XF86AudioRaiseVolume", lazy.spawn("/home/josema/scripts/volume.sh up")),
+    Key([],"XF86AudioMute", lazy.spawn("amixer set Master 0%")),
 
     # Brightness
-    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set 10%+")),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("/home/josema/scripts/brightness.sh up")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("/home/josema/scripts/brightness.sh down")),
+
+    # Screenshot
+    Key([mod], "s", lazy.spawn("scrot /home/josema/images/screen_shot")),
 ]
