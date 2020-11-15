acb58e3eb00d30064cfb01996b33015c84bfd5d5
josemapt <73686197+josemapt@users.noreply.github.com>
2020-11-15 11:34:44 +0100

    Update layouts.py

--- .config/qtile/settings/layouts.py
+++ .config/qtile/settings/layouts.py
@@ -1,29 +1,15 @@
 from libqtile import layout
 
 layouts = [
+    layout.Max(
+        margin = 6,
+    ),
     layout.MonadTall(
         margin = 6,
         border_focus = "#FFFFFF",
     ),
-    layout.Max(
+    layout.MonadWide(
         margin = 6,
+        border_focus = "#FFFFFF",
     ),
 ]
-
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'confirmreset'},  # gitk
-    {'wmclass': 'makebranch'},  # gitk
-    {'wmclass': 'maketag'},  # gitk
-    {'wname': 'branchdialog'},  # gitk
-    {'wname': 'pinentry'},  # GPG key password entry
-    {'wmclass': 'ssh-askpass'},  # ssh-askpass
-])
