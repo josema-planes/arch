3ca4914b98dec722aa3c679b9434020bbdf25abd
josemapt <73686197+josemapt@users.noreply.github.com>
2020-11-16 14:34:47 +0100

    Update topbar.py

--- .config/qtile/settings/topbar.py
+++ .config/qtile/settings/topbar.py
@@ -1,22 +1,33 @@
 from libqtile import bar, layout, widget
 from libqtile.config import Screen
 
+from settings.colors import colors
+
 widget_defaults = dict(
     font='UbuntuMono Nerd Font Bold',
     fontsize=24,
     padding=4,
+    background = colors[7],
 )
+
 extension_defaults = widget_defaults.copy()
 
-# Functions
+# Functions -------------------------------------------
 def open_demenu(qtile):
     qtile.cmd_spawn('dmenu_run')
 
 def run_redshift(qtile):
     qtile.cmd_spawn('/home/josema/scripts/redshift.sh')
 
+def shutdown(qtile):
+    qtile.cmd_to_layout_index(3)
+    qtile.cmd_spawn('/home/josema/scripts/shutdown.py')
 
-# Widgets
+def reload_qtile(qtile):
+    qtile.cmd_simulate_keypress(["mod4", "shift"], "r")
+
+
+# Widgets -------------------------------------------
 screens = [
     Screen(
         top=bar.Bar(
@@ -24,116 +35,102 @@ screens = [
                 widget.GroupBox(
                     fontsize = 35,
                     padding = 10,
-                    borderwidth = 3,
-                    active = "#FFFFFF",
+                    margin_y = -1,
+                    borderwidth = 2,
+                    active = colors[4],
+                    inactive = colors[5],
                     rounded = False,
                     highlight_method = "block",
-                    this_current_screen_border = "#AA4444",
-                    disable_drag=True
+                    this_current_screen_border = colors[6],
+                    disable_drag=True,
                     ),
 
                 widget.Prompt(),
 
-                widget.WindowName(
-                    fontsize = 14,
-                    foreground = "#a151d3",
-                    ),
+                widget.Spacer(),
 
                  widget.TextBox(
                     text=" ",
                     fontsize=37,
                     padding=-2,
-                    background = "#000000",
-                    foreground = "#fb9f7f",
+                    background =  colors[7],
+                    foreground = colors[2],
                     ),
+                
+                widget.CurrentLayoutIcon(
+                    scale = 0.7,
+                    background = colors[2],
+                ),
 
                 widget.CurrentLayout(
-                    background = "#fb9f7f",
+                    background = colors[2],
                 ),
 
                 widget.TextBox(
                     text=" ",
                     fontsize=37,
                     padding=-2,
-                    background = "#fb9f7f",
-                    foreground = "#F07178",
+                    background = colors[2],
+                    foreground = colors[1],
                     ),
                 
                 widget.TextBox(
                     text = "",
                     fontsize = 16,
-                    background = "#F07178",
+                    background = colors[1],
                 ),
 
                 widget.Net(
                     interface = "wlo1",
                     format = '{down}↓{up}↑',
-                    background = "#F07178",
+                    background = colors[1],
                     ),
 
                 widget.TextBox(
                     text=" ",
                     fontsize=37,
                     padding=-2,
-                    background = "F07178",
-                    foreground = "#a151d3",
+                    background = colors[1],
+                    foreground = colors[0],
                     ),
 
                 widget.TextBox(
                     text = " ",
                     fontsize = 16,
-                    background = "#a151d3",
+                    background = colors[0],
                 ),
 
                 widget.Clock(
-                    format = '%I:%M %p',
-                    background = "#a151d3",
+                    format = "%A, %B %d  [ %H:%M ]",
+                    background = colors[0],
                     ),
-
-                #widget.TextBox(text = "| ♪"),
-                #widget.Volume(),
-
-                #widget.TextBox(
-                #    text = "| ",
-                #    fontsize = 16
-                #    ),
-
-                #widget.Battery(
-                #    format = '{char} {percent:2.0%}',
-                #    charge_char = "⚡",
-                #    discharge_char = " ",
-                #    notify_below = 20,
-                #    low_foreground = 'FF0000',
-                #    low_percentage = 0.3,
-                #    update_interval = 30,
-                #    ),
-
-                widget.TextBox(
-                    text=" ",
-                    fontsize=37,
-                    padding=-2,
-                    background = "#a151d3",
-                    foreground = "#000000",
-                    ),
-
-                widget.TextBox(
-                    text = " ",
-                    fontsize = 16,
-                    mouse_callbacks = {'Button1': open_demenu},
-                ),
-                
-                widget.TextBox(
-                    text = "☽",
-                    fontsize = 18,
-                    mouse_callbacks = {'Button1': run_redshift},
-                ),
-                
-                widget.Systray(
-                    icon_size = 24,
-                    padding = 8,
-                ),
             ],
             24,
         ),
+        bottom=bar.Bar([
+
+            widget.WindowName(
+                    fontsize = 16,
+                    foreground = colors[4],
+                    ),
+            
+            widget.TextBox(
+                text = "  ",
+                fontsize = 18,
+                mouse_callbacks = {'Button1': run_redshift}
+            ),
+
+            widget.Systray(
+                    icon_size = 28,
+                    padding = 8,
+                ),
+
+                widget.TextBox(
+                    text = "  ",
+                    fontsize = 16,
+                    mouse_callbacks = {'Button1': shutdown},
+                ),
+        ],
+        30),
     ),
 ]
