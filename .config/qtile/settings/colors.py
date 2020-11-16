bc4d3dc3c40bade59e52be9f03616226070ae5da
josemapt <73686197+josemapt@users.noreply.github.com>
2020-11-16 14:33:27 +0100

    Add files via upload

---
+++ .config/qtile/settings/colors.py
@@ -0,0 +1,15 @@
+colors = [
+    # 0 Powerlines------
+    "#a151d3",  # Purple
+    "#F07178",  # Orange
+    "#fb9f7f",  # Yellow / orange
+    "#ffd47e",  # Yellow
+
+    # 4 Groupbox--------
+    "#f1ffff",  # Active
+    "#4c566a",  # Inactive
+    "#ff5555",  # Current goup
+
+    # 7 Satus_bar-------
+    "#292d3e",  # Background
+]
