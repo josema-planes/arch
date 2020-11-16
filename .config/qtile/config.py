2464da320e11b226518a657bbd2bd89bba4a5aa6
josemapt <73686197+josemapt@users.noreply.github.com>
2020-11-16 14:32:33 +0100

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,14 +1,18 @@
+from os import path
+import re
+import socket
 import subprocess
 from libqtile import hook
 
 from settings.keys import keys, mod
 from settings.groups import groups
 from settings.topbar import screens, widget_defaults, extension_defaults
-from settings.layouts import layouts, floating_layout
-# from settings.mouse import mouse
+from settings.layouts import layouts
 from settings.path import qtile_path
-
 
 @hook.subscribe.startup_once
 def autostart():
-    subprocess.call([path.join(qtile_path)])
+    subprocess.call([path.join(qtile_path, 'autostart.sh')])
+
+bring_front_click = True
+focus_on_window_activation = "focus"
