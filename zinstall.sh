093e897a845046e4baf05ad453de1a7d46fbe18e
josemapt <73686197+josemapt@users.noreply.github.com>
2020-12-09 12:59:36 +0100

    Update zinstall.sh

--- zinstall.sh
+++ zinstall.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-echo "setting up qtile and zsh config"
+echo "\033[0;33mSetting up qtile and zsh config\033[0m"
+
+echo "\033[0;33mInstalling necessary pakages\033[0m"
+sudo pacman -S thunar nitrogen dmenu git brightnessctl python-psutil acpi alsa-utils volumeicon cbatticon network-manager-applet geeqie xcb-util-cursor xf86-video-intel xf86-video-nouveau exa gvfs ntfs-3g dunst scrot redshift bc unzip evince zsh
+echo "\033[0;33mDone\033[0m"
+
+echo "\033[0;33mInstalling yay\033[0m"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+echo "\033[0;33mDone\033[0m"
+
+echo "\033[0;33mCloning repository\033[0m"
+git clone https://github.com/josemapt/dotfiles.git
+
+echo "\033[0;33mRelocating files\033[0m"
 cd ~
 mv -f dotfiles/.config/qtile/* .config/qtile
 chmod +x .config/qtile/autostart.sh
@@ -18,5 +34,4 @@ mv dotfiles/.zshrc ~
 chsh -s /bin/zsh $(whoami)
 sudo -s /bin/zsh
 rm -r dotfiles
-
-echo "done"
+echo "\033[0;33mDone\033[0m"
