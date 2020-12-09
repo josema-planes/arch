e2bf38245dc6c82c7c40ac0decde714fd328efe3
josemapt <73686197+josemapt@users.noreply.github.com>
2020-12-09 14:49:57 +0100

    Update zinstall.sh

--- zinstall.sh
+++ zinstall.sh
@@ -14,6 +14,10 @@ echo "${YE}Done${NC}"
 echo -n -e "${PU}Do you want to install yay (Y/n)?${NC} "
 read answer1
 if [ "$answer1" = "y" ] || [ "$answer1" = "" ] ;then
+    YE='\033[0;33m'
+    PU='\033[0;35m'
+    NC='\033[0m'
+    
     echo "${YE}Installing yay...${NC}"
     sleep 1
     git clone https://aur.archlinux.org/yay.git
@@ -56,26 +60,30 @@ echo -n -e "${PU}Do you want to install the Breeze cursor theme (needed to be do
 read answer2
 
 if [ "$answer2" = "y" ] || [ "$answer1" = "" ] ;then
+    YE='\033[0;33m'
+    PU='\033[0;35m'
+    NC='\033[0m'
+
     cd Downloads/
     ex 165371-Breeze.tar.gz
     rm 165371-Breeze.tar.gz
     sudo mv Breeze /usr/share/icons
     
+    mkdir .config/gtk-3.0
     touch .config/gtk-3.0/settings.ini
     echo "[Settings]" >> .config/gtk-3.0/settings.ini
     echo "gtk-cursor-theme-name = Breeze" >> .config/gtk-3.0/settings.ini
     
-    sudo sed 'Inherits=Adwaita' /usr/share/icons/default/index.theme
-    sudo echo "Inherits=Breeze" >> /usr/share/icons/default/index.theme
+    sudo sed -i 's/Adwaita/Breeze/g' /usr/share/icons/default/index.theme
 fi
 
-echo -n -e "${PU}Do you want to set zsh as default shell (Y/n)?${NC} "
-read answer3
+#echo -n -e "${PU}Do you want to set zsh as default shell (Y/n)?${NC} "
+#read answer3
 
-if [ "$answer3" = "y" ] || [ "$answer1" = "" ] ;then
-    chsh -s /bin/zsh $(whoami)
-    sudo -s /bin/zsh
-fi
+#if [ "$answer3" = "y" ] || [ "$answer1" = "" ] ;then
+#    chsh -s /bin/zsh $(whoami)
+#    sudo -s /bin/zsh
+#fi
 
 
 echo -n -e "${YE}Setting up completed. ${PU}Do you want to reboot now (Y/n)?${NC} "
