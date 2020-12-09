a979e6e782c1506db94f24484acd18003600a315
josemapt <73686197+josemapt@users.noreply.github.com>
2020-12-09 19:29:18 +0100

    Update zinstall.sh

--- zinstall.sh
+++ zinstall.sh
@@ -21,7 +21,8 @@ echo -e "${YE}Done${NC}"
 cd ~
 echo -e "${YE}Installing yay pakages...${NC}"
 sleep 1
-yay -S --noconfirm vscodium-bin nerd-fonts-ubuntu-mono ccat
+
+yay -S vscodium-bin nerd-fonts-ubuntu-mono ccat
 
 echo -e "${YE}Cloning repository...${NC}"
 git clone https://github.com/josemapt/dotfiles.git
